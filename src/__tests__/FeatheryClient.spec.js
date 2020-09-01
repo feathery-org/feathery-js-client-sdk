c944f55c15e87d37501bc7890a5b396d9dba3a18
Buck Tower <buck@bucktower.net>
2020-09-01 01:21:49 -0700

    Begin work on writing a jest test for fetching panel

--- src/__tests__/FeatheryClient.spec.js
+++ src/__tests__/FeatheryClient.spec.js
@@ -39,3 +39,67 @@ describe("FeatheryClient", () => {
             });
     });
 });
+
+describe("FeatheryClient", () => {
+    global.fetch_panel = jest.fn(() => {
+        return Promise.resolve({
+            status: 200,
+            json: {
+                "header": "Welcome to Zendesk",
+                "description": "We serve 2,000 academic institutions worldwide",
+                "servars": [
+                  {
+                    "id": "91a963fd-23ba-4ef9-bb21-14740319dd60",
+                    "name": "What's your role?",
+                    "key": "role",
+                    "type": "select",
+                    "metadata": {
+                      "options": [
+                        "School Administrator",
+                        "Executive Assistant",
+                        "IT"
+                      ]
+                    },
+                    "value": "School Administrator"
+                  },
+                  {
+                    "id": "e426fb0b-58d4-4519-b955-95fa054a53ca",
+                    "name": "What educational stage does your school serve?",
+                    "key": "educational_stage",
+                    "type": "select",
+                    "metadata": {
+                      "options": [
+                        "University",
+                        "Grade School",
+                        "Preschool"
+                      ]
+                    },
+                    "value": "University"
+                  },
+                  {
+                    "id": "6f474b27-3dd9-4d0e-b24d-7897ee12cd67",
+                    "name": "What are you using Zendesk for?",
+                    "key": "use",
+                    "type": "multiple_checkbox",
+                    "metadata": {
+                      "options": [
+                        "Managing student support tickets",
+                        "Recruiting prospective students",
+                        "Engage students in communication"
+                      ]
+                    },
+                    "value": "[False, False, False]"
+                  }
+                ]  
+              }
+        })
+    })
+
+    it("panel", async done => {
+        const client = new FeatheryClient(orgId, userKey)
+        await client.resolve
+            .then(() => {
+                console.log()
+            })
+    })
+})
